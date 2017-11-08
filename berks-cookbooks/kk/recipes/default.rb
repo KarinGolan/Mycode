8b4eb804b95c55c7f12b68a973fb0bb20b85f10f
Karin Golan <Karingolan25@gmail.com>
2017-11-08 12:50:44 +0100

    new change to kk recipe -the all project

--- berks-cookbooks/kk/recipes/default.rb
+++ berks-cookbooks/kk/recipes/default.rb
@@ -22,3 +22,30 @@ execute "copying output file to s3" do
 command "aws s3 cp /home/ec2-user/output.txt s3://myautomationscailing/myfolder/output.txt"
  action "run"
 end
+
+bash "running bash script" do
+code 'FILE="output.txt"; if [ -f "/home/ec2-user/$FILE" ];  then NOW=$(date +"%c");  echo "Current time : $NOW", `curl http://169.254.169.254/latest/meta-data/instance-id`, "created successfully" >> /home/ec2-user/output.txt ; else echo "File $FILE does not exist";  fi'
+end
+
+execute "download file from  s3" do
+command "aws s3 cp s3://myautomationscailing/myfolder/newfile.txt  /home/ec2-user/newfile.txt"
+ action "run"
+end
+
+
+
+execute "give permmission" do
+command "sudo chmod 777 /home/ec2-user/newfile.txt"
+ action "run"
+end
+
+
+execute "copy details" do
+command "cat /home/ec2-user/output.txt >> /home/ec2-user/newfile.txt"
+ action "run"
+end
+
+execute "upload new file with details " do
+command "aws s3 cp /home/ec2-user/newfile.txt s3://myautomationscailing/myfolder/newfile.txt"
+ action "run"
+end
